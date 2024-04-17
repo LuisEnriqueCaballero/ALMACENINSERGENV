e41e1d6e037bea4012c684a0619325be21cd8ff2
LuisEnriqueCaballero <enrique.caballero.0602@gmail.com>
2024-04-17 01:29:17 -0500

    backend controlladores

---
+++ Controller/Controllproveedores.php
@@ -0,0 +1,31 @@
+<?php
+include_once '../metodo/MetodoProveedor.php';
+$metodoproveedor = new MetodoProveedor();
+$ope = isset($_GET['ope']) ? $_GET['ope'] : '';
+switch ($ope) {
+    case '1':
+        # code...
+        break;
+    case '2':
+        # code...
+        break;
+    case '3':
+        # code...
+        break;
+    case '4':
+        # code...
+        break;
+    case '5':
+        # code...
+        break;
+    case '6':
+        # code...
+        break;
+    case '7':
+        # code...
+        break;
+
+    default:
+        # code...
+        break;
+}
